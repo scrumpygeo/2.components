381e89d13d9dfa58fb1542592b3fc54c14922b73
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-02 11:59:53 +0000

    finalize basic framework of 3 posts by bulky duplication

--- src/index.js
+++ src/index.js
@@ -19,6 +19,36 @@ const App = () => {
           <div className="text">Very interesting take on it!</div>
         </div>
       </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Joseph
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:45PM</span>
+          </div>
+          <div className="text">Not really a conspiracy theorist.</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Shelley
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:45PM</span>
+          </div>
+          <div className="text">Meh!</div>
+        </div>
+      </div>
     </div>
   );
 };
