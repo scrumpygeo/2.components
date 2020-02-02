262faab0a758e1f9356ed13ba56a76589278ec98
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-02 11:41:58 +0000

    add avatar images with faker.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
-  return <div>Yoo-hoo!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Frederico
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:45PM</span>
+          </div>
+          <div className="text">Very interesting take on it!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
